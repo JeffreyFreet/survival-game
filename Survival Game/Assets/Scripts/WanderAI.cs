2754fb59939db1a8895da2aea0e3526086d6ff00
Jeffrey Freet <jpfvx8@mail.missouri.edu>
2018-12-10 11:13:17 -0500

    Wandering AI added
    Started making mobs

---
+++ Survival Game/Assets/Scripts/WanderAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderAI : MonoBehaviour
+{
+    [SerializeField]
+    float Speed = 3;
+
+    Vector3 wayPoint;
+
+    [SerializeField]
+    int Range = 10;
+
+    void Start()
+    {
+        wander();
+    }
+
+    void Update()
+    {
+        transform.position += transform.TransformDirection(Vector3.forward) * Speed * Time.deltaTime;
+        if ((transform.position - wayPoint).magnitude < 3)
+        {
+            wander();
+        }
+    }
+    
+    //Creates a waypoint for the mob to walk to
+    void wander()
+    {
+        wayPoint = new Vector3(Random.Range(transform.position.x - Range, transform.position.x + Range), 1, Random.Range(transform.position.z - Range, transform.position.z + Range));
+        wayPoint.y = 1;
+        transform.LookAt(wayPoint);
+    }
+}
