ef37f320c73e545156807fad9dd32f5069a5bb36
Jeffrey Freet <jpfvx8@mail.missouri.edu>
2018-12-19 16:44:06 -0500

    AI Work Done
    Work done on first real mob in the game

---
+++ Survival Game/Assets/Scripts/NPC/Bear.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : Creature {
+
+    private GameObject target;
+
+    // Use this for initialization
+    void Start()
+    {
+        name = "Bear";
+        health = 15;
+        maxHealth = health;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (health <= 0)
+            Death();
+
+        UpdateUI();
+
+        transform.position += transform.TransformDirection(Vector3.forward) * Speed * Time.deltaTime;
+        if ((transform.position - wayPoint).magnitude < 3)
+        {
+            Wander();
+        }
+
+        if ((transform.position - player.transform.position).magnitude < 10)
+            target = player;
+
+        if ((transform.position - player.transform.position).magnitude > 15)
+            target = null;
+
+        if (target != null)
+        {
+            wayPoint = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+            transform.LookAt(wayPoint);
+        }
+    }
+
+    protected override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Death()
+    {
+        Drop();
+        Destroy(this.gameObject);
+    }
+
+    protected override void Drop()
+    {
+        Instantiate(drop, new Vector3(transform.position.x, 0, transform.position.z), Quaternion.identity);
+    }
+
+    public override void Interact()
+    {
+        health--;
+    }
+
+}
--- Survival Game/Assets/Scripts/NPC/Creature.cs
+++ Survival Game/Assets/Scripts/NPC/Creature.cs
@@ -8,15 +8,35 @@ public abstract class Creature : MonoBehaviour {
     protected string name;
     protected int health;
     protected int maxHealth;
-    [SerializeField]
     protected GameObject player;
     public Image healthBar;
     public Image healthBG;
+    public GameObject drop;
 
     public abstract void Interact();
-    public abstract void Attack(); //Only neccesary on hostiles
-    public abstract void Death();
-    public abstract void Drop();
+    protected abstract void Attack(); //Only neccesary on hostiles
+    protected abstract void Death();
+    protected abstract void Drop();
+
+    protected Vector3 wayPoint;
+    protected float Speed = 3;   
+    protected int Range = 10;
+
+    protected void Wander()
+    {
+        wayPoint = new Vector3(Random.Range(transform.position.x - Range, transform.position.x + Range), 
+                               0, 
+                               Random.Range(transform.position.z - Range, transform.position.z + Range));
+        transform.LookAt(wayPoint);
+    }
+
+    protected void UpdateUI()
+    {
+        healthBar.transform.LookAt(player.transform);
+        healthBG.transform.LookAt(player.transform);
+        healthBar.fillAmount = (float)health / (float)maxHealth;
+        healthBG.fillAmount = 1 - healthBar.fillAmount;
+    }
 
 
 }
